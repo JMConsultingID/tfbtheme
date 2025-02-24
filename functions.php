9a7776985da023b9ee59340d354e87b250b9f11e
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2025-02-24 20:48:55 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -18,6 +18,32 @@ define('TFB_THEME_VERSION', '1.1.1');
 add_filter('woocommerce_order_pay_need_login', '__return_false');
 
 /**
+ * Pay for Order or Subscription if Logged Out - WooCommerce Checkout
+ */
+function tfb_log_in_user_from_payment_link(){
+    global $wp;
+    if ( isset( $_GET['pay_for_order'] ) && isset( $_GET['key'] ) && isset( $wp->query_vars['order-pay'] ) && ! is_user_logged_in() ) {
+        $order_key = $_GET['key'];
+        $order_id  = isset( $wp->query_vars['order-pay'] ) ? $wp->query_vars['order-pay'] : absint( $_GET['order_id'] );
+        $order     = wc_get_order( $order_id );
+        $user_id   = $order->get_user_id();
+        $user      = get_user_by('id', $user_id);
+
+        // Check if order key corresponds with order and the status of the payment is pending or failed
+        if ( wcs_get_objects_property( $order, 'order_key' ) === $order_key && $order->has_status( array( 'pending', 'failed' ) ) ) {
+            // For security reasons we want to limit this function to customers and subscribers
+            if ( in_array( 'customer', $user->roles ) || in_array( 'subscriber', $user->roles ) ) {
+                // Log the user in
+                wp_set_auth_cookie($user_id);
+                wp_set_current_user($user_id);
+            }
+        }
+        
+    }
+}
+add_action( 'send_headers', 'tfb_log_in_user_from_payment_link', 0 );
+
+/**
  * Load tfb theme scripts & styles.
  *
  * @return void
